8912a4d213608944707ac8fa493730e92e8c7b1e
Skan22 <skanderyacoubi2005@gmail.com>
2026-08-16 15:51:39 +0100

    Added tests

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+
+func Test_api_key(t *testing.T){
+	header := http.Header{}
+	header.Add("Authorization","ApiKey ezfhiaeohioaf&é1365")
+	apiKey,err := GetAPIKey(header)
+	if err!=nil {
+		t.Errorf("expected error to be %v , got  : %v",nil,err)
+	}
+	if apiKey!="ezfhiaeohioaf&é1365_"{
+		t.Errorf("expected %s , got : %s","ezfhiaeohioaf&é1365",apiKey)
+	}
+	
+}
