fc5c2e14d3fd844278c9358d3593aeace9bcf14a
Skan22 <skanderyacoubi2005@gmail.com>
2026-08-16 15:57:57 +0100

    Added tests

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -5,16 +5,15 @@ import (
 	"testing"
 )
 
-
-func Test_api_key(t *testing.T){
+func Test_api_key(t *testing.T) {
 	header := http.Header{}
-	header.Add("Authorization","ApiKey ezfhiaeohioaf&é1365")
-	apiKey,err := GetAPIKey(header)
-	if err!=nil {
-		t.Errorf("expected error to be %v , got  : %v",nil,err)
+	header.Add("Authorization", "ApiKey ezfhiaeohioaf&é1365")
+	apiKey, err := GetAPIKey(header)
+	if err != nil {
+		t.Errorf("expected error to be %v , got  : %v", nil, err)
 	}
-	if apiKey!="ezfhiaeohioaf&é1365_"{
-		t.Errorf("expected %s , got : %s","ezfhiaeohioaf&é1365",apiKey)
+	if apiKey != "ezfhiaeohioaf&é1365_dffez" {
+		t.Errorf("expected %s , got : %s", "ezfhiaeohioaf&é1365", apiKey)
 	}
-	
+
 }
